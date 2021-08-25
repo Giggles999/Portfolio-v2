ffc55c796921897d74b62788a103cd1dab8d844c
Lynn M <78457709+Giggles999@users.noreply.github.com>
2021-08-24 22:01:38 -0700

    add toggle menu and js

---
+++ src/index.js
@@ -0,0 +1,12 @@
+document.addEventListener("DOMContentLoaded", function (event) {
+    navbarFixedTopAnimation();
+    closeMenuBeforeGoingToPage();
+  });
+
+document.addEventListener("DOMContentLoaded", function (event) {
+    navbarToggleSidebar();
+    closeMenuBeforeGoingToPage();
+    navActivePage();
+  });
+
+
